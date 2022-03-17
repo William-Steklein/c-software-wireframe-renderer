030b86a0c3aa708236f3d75693cb84419a3a5e42
William <st.william24@gmail.com>
2022-03-17 13:24:13 +0100

    separation of main and engine

---
+++ src/engine.c
@@ -0,0 +1,41 @@
+#include "engine.h"
+
+void run() {
+    // Initialize SDL
+    SDL_Init(SDL_INIT_VIDEO);
+
+    // Create a SDL window
+    SDL_Window *window = SDL_CreateWindow("Hello, SDL2", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, width, height, SDL_WINDOW_OPENGL);
+
+    // Create a renderer (accelerated and in sync with the display refresh rate)
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+
+//    SDL_SetRenderDrawColor(renderer, 255, 0, 255, 255);
+
+    int running = 1;
+    SDL_Event event;
+    while(running)
+    {
+        // Process events
+        while(SDL_PollEvent(&event))
+        {
+            if(event.type == SDL_QUIT)
+            {
+                running = 0;
+            }
+        }
+
+        // Clear screen
+        SDL_RenderClear(renderer);
+
+        // Draw
+
+        // Show what was drawn
+        SDL_RenderPresent(renderer);
+    }
+
+    // Release resources
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+}
---
+++ src/engine.h
@@ -0,0 +1,11 @@
+#ifndef C_TEST_ENGINE_H
+#define C_TEST_ENGINE_H
+
+#include <SDL2/SDL.h>
+
+static const int width = 1024;
+static const int height = 1024;
+
+void run();
+
+#endif //C_TEST_ENGINE_H
